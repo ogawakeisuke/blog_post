ab08a43ceb64c75f52b1de44dbe4d9c5d3733b6f
ogawakeisuke <ogawa@uniba.jp>
2013-04-01 18:11:15 +0900

    Xmlsクラスだんだん出来て来た

--- lib/xmls/fc2.rb
+++ lib/xmls/fc2.rb
@@ -13,21 +13,6 @@ module Xmls
 
   class Fc2
 
-    # client = XMLRPC::Client.new2(FLICKR_SERVER)
-    # begin
-    #   result = client.call("flickr.photos.getRecent",{"api_key"=>API_KEY,"page"=>1,"per_page"=>5})
-    #   xml = XmlSimple.xml_in(result)
-    #   xml["photo"].each { |photo|
-    #     puts "http://farm#{photo["farm"]}.static.flickr.com/#{photo["server"]}/#{photo["id"]}_#{photo["secret"]}.jpg"
-    #   }
-
-    # rescue XMLRPC::FaultException => error
-    # p  error.faultCode
-    # p  error.faultString
-    # end
-
-
-
     def self.blogpost(title, description, blogid = SetConst::APIID, user = SetConst::APINAME, passwd = SetConst::APIPASS)
       content = {
         "title" => title, 
@@ -35,17 +20,46 @@ module Xmls
         # "mt_tb_ping_urls" => tburls, 
         # "mt_excerpt" => excerpt 
       }
+      #TODO ここは独立させるべき
       client = XMLRPC::Client.new("blog.fc2.com", "/xmlrpc.php")
       begin 
         r = client.call("metaWeblog.newPost", blogid, user, passwd, content, true)
-        p r
       rescue XMLRPC::FaultException => error
-        p  error.faultCode
-        p  error.faultString
+        # p  error.faultCode
+        # p  error.faultString
+        return false
       end
+    
     end
 
+    def self.imagepost(file_path, blogid = SetConst::APIID, user = SetConst::APINAME, passwd = SetConst::APIPASS)
+      filename = File.basename(file_path)
 
+      p filename
+      open(filename, 'wb') do |file|
+        open(file_path) do |data|
+          file.write(data.read)
+        end
+      end
+
+      base64 = XMLRPC::Base64.new(File.read(filename))
+      fileHash = {
+        :name => "#{Time.now.to_i}.png",
+        :bits => base64
+      }
+      
+      client = XMLRPC::Client.new("blog.fc2.com", "/xmlrpc.php")
+
+      begin 
+        r = client.call("metaWeblog.newMediaObject",blogid, user, passwd,fileHash)
+      rescue XMLRPC::FaultException => error
+        # p  error.faultCode
+        # p  error.faultString
+        return false
+      end
+    
+      return r.url
+    end
 
   end
 end
